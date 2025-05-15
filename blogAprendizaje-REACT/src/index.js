2fd890e451f390d779e03554f0d9633f1e0677f6
dmarroquin-2023076 <dmarroquin-2023076@kinal.edu.gt>
2025-05-14 23:49:47 -0600

    Se agrega funcionalidad al index.js

---
+++ blogAprendizaje-REACT/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+// Importar Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Importar Bootstrap JS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
