6cdaffffde435ebf94b50fb043ff8d32acee5e33
dmarroquin-2023076 <dmarroquin-2023076@kinal.edu.gt>
2025-05-18 18:18:53 -0600

    Se cambian comentarios especificos y se esperan cambios en la parte visual.

--- blogAprendizaje-REACT/src/main.jsx
+++ blogAprendizaje-REACT/src/main.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-// Importar Bootstrap CSS - si usas Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
-// Importar Bootstrap JS - si necesitas los componentes JS de Bootstrap
 import 'bootstrap/dist/js/bootstrap.bundle'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
